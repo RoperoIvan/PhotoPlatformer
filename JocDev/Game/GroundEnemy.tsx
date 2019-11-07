b9d5898bf0cc052530391302e74d2d7507bddf53
RoperoIvan <ivan_ropero98@hotmail.com>
2019-11-07 18:42:27 +0100

    GroundEnemy added

---
+++ JocDev/Game/GroundEnemy.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="GroundEnemy" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="textures/Slime_Medium_White.png" width="128" height="128"/>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
